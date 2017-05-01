44843afe50a3e2e92c6fda2a3d05a153d4e0b280
rastasimonbonface <rastasimonbonface@gmail.com>
2017-05-01 14:56:52 +0200

    you forgot one .org

--- ownerdelete.user.js
+++ ownerdelete.user.js
@@ -4,7 +4,7 @@
 // @version      0.10
 // @description  try to take over the world!
 // @author       You
-// @match        https://volafile.io/r/*
+// @match        https://volafile.org/r/*
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
